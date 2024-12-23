3cef58144e8b59cfc4b0f27722723d96d33c1d3c
maksymilianzmijewski <maksymilianzmijewski1@gmail.com>
2024-12-23 20:46:49 +0100

    init commit

---
+++ src/main/java/com/zmijewskimaksymilian/social_wall/SocialWallApplication.java
@@ -0,0 +1,13 @@
+package com.zmijewskimaksymilian.social_wall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialWallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zmijewskimaksymilian/social_wall/SocialWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmijewskimaksymilian.social_wall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialWallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
